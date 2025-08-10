688e6cc008703e9dfb694f1a887a26549cef4ff4
Sagar Bhor <sagarrbhor715@gmail.com>
2025-08-10 17:04:26 +0530

    commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,16 +1,65 @@
-import {Link} from "react-router-dom"; 
+import { useState } from "react";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    gender: "",
+    age: "",
+    contactNumber: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const navigate = useNavigate();
+  navigate("/login");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await axios.post(
+        "http://localhost:8080/api/users/register?",
+        formData,
+        {
+          params: {
+            isAdmin: false,
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast.success("Login successful");
+      navigate("/login");
+    } catch (error) {
+      toast.error("Sign Up Failed");
+    }
+  };
+
   return (
     <div className="auth-container ">
+      <ToastContainer />
       <h2 className="text-center mb-4">Sign Up</h2>
-      <form className="auth-form">
+      <form onSubmit={handleSubmit} className="auth-form">
         <div className="mb-3">
           <label>Full Name</label>
           <input
             type="text"
             className="form-control"
             placeholder="Enter name"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
           />
         </div>
 
@@ -20,6 +69,9 @@ function Signup() {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
           />
         </div>
 
@@ -29,12 +81,20 @@ function Signup() {
             type="password"
             className="form-control"
             placeholder="Create password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
           />
         </div>
 
         <div className="mb-3">
           <label>Gender</label>
-          <select className="form-control">
+          <select
+            name="gender"
+            className="form-control"
+            value={formData.gender}
+            onChange={handleChange}
+          >
             <option value="">Select gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
@@ -48,6 +108,9 @@ function Signup() {
             type="number"
             className="form-control"
             placeholder="Enter age"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
           />
         </div>
 
@@ -57,6 +120,9 @@ function Signup() {
             type="number"
             className="form-control"
             placeholder="Enter contact number"
+            name="contactNumber"
+            value={formData.contactNumber}
+            onChange={handleChange}
           />
         </div>
 
